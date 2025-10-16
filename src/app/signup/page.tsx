0c904ab8b795c66f07a39f7a5f663005c77f36be
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-10-15 23:02:03 -0400

    finish design of signup page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,26 @@
+import Logo from "@/ui/components/logo";
+import { Input } from "@/ui/shadcn/input";
+import { Label } from "@/ui/shadcn/label";
+import Link from "next/link";
+
+export default function Page(){
+  return (
+    <>
+      <main className="flex bg-nl-100 w-screen h-screen justify-center items-center dark:bg-nd-900">
+        <form action="" className="bg-neutral py-10 px-8 h-fit rounded-xl w-full max-w-112 mx-4 shadow-modall">
+          <Logo />
+          <h1 className="text-nl-900 text-2xl font-bold mt-8">Create your Account</h1>
+          <p className="text-nl-800 text-sm font-medium mt-1.5">Join us and start saving your favorite links — organized, searchable, and always within reach.</p>
+          <Label htmlFor="sname" className="mt-8">Full name<span className="text-teal-700">&nbsp;*</span></Label>
+          <Input id="sname" className="mt-1.5"/>
+          <Label htmlFor="semail" className="mt-4">Email Address<span className="text-teal-700">&nbsp;*</span></Label>
+          <Input id="semail" className="mt-1.5"/>
+          <Label htmlFor="spass" className="mt-4">Password<span className="text-teal-700">&nbsp;*</span></Label>
+          <Input id="spass" className="mt-1.5"/>
+          <button className="w-full mt-4 px-4 py-3 border-2 bg-teal-700 text-neutral text-base/[1.4] font-semibold rounded-lg cursor-pointer hover:bg-teal-800">Create Account</button>
+          <p className="text-nl-800 mt-8 text-center text-sm font-medium">Already have an account? <Link href={"/login"} className="font-semibold leading-[1.4] text-nl-900 hover:text-teal-700">Log In</Link></p>
+        </form>
+      </main>
+    </>
+  );
+}
---
+++ src/ui/shadcn/label.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import * as React from "react"
+import * as LabelPrimitive from "@radix-ui/react-label"
+
+import { cn } from "@/lib/utils"
+
+function Label({
+  className,
+  ...props
+}: React.ComponentProps<typeof LabelPrimitive.Root>) {
+  return (
+    <LabelPrimitive.Root
+      data-slot="label"
+      className={cn(
+        "items-center gap-2leading-none select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50 block text-nl-900 text-sm font-semibold cursor-pointer",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+export { Label }
