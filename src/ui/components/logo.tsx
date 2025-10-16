80c8caef5f66de5c925c2fd9c5752b043e555610
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-10-15 20:12:57 -0400

    add login page

---
+++ src/ui/components/logo.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useState } from "react";
+import logoLight from "../../../public/svgs/icon-light-theme.svg";
+import logoDark from "../../../public/svgs/logo-dark-theme.svg";
+
+export default function Logo(){
+
+  const [logo, setLogo] = useState(logoLight);
+
+  return (
+    <>
+
+    </>
+  );
+}
